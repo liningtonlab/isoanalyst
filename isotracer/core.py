edde6f604ad121ba5d74e573ec982ae908be9308
Jeff van Santen <jeffreyavansanten@gmail.com>
2021-02-09 11:02:25 -0800

    Fix 15N condition check

--- isotracer/core.py
+++ isotracer/core.py
@@ -120,7 +120,7 @@ def isotope_scraper(
         # loops through all unique ions in the master list and iteratively adds
         # Set of seen indices in func_df, updated in isotope_slicer
         # disable N15 searching and use wider tolerance
-        iso = "15N" if cond in ("GLU", "GLN") else "13C"
+        iso = "15N" if any(c in cond for c in ("GLU", "GLN")) else "13C"
 
         len_uni = len(master.Exp_ID.unique())
         exps = master.groupby("Exp_ID")
